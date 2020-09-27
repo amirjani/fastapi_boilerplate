3bc7cdcc612accb001dd0f740b162c484f8da79f
Amirhossein Jani <amir.jani500@gmail.com>
2020-09-27 22:37:16 +0330

    initialized

---
+++ app/core/config.py
@@ -0,0 +1,54 @@
+import secrets
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    FIRST_SUPERUSER: str = "amir.jani500@gmail.com"
+    FIRST_SUPERUSER_PASSWORD: str = "123123"
+
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SERVER_NAME: str = 'localhost'
+    SERVER_HOST: AnyHttpUrl = 'http://www.localhost:3000'
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:4200", "http://localhost:3000"]
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str = 'AppEscort'
+
+    POSTGRES_SERVER: str = 'localhost'
+    POSTGRES_USER: str = 'amirjani'
+    POSTGRES_PASSWORD: str = 'amirjani'
+    POSTGRES_DB: str = 'AppEscort'
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = 'postgresql://amirjani:amirjani@localhost/AppEscort'
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ prestart.sh
@@ -0,0 +1,5 @@
+python3.8 ~/Sites/AppEs/prestart.py
+
+PYTHONPATH=. alembic upgrade head
+
+python3.8 ~/Sites/AppEs/initial_data.py
