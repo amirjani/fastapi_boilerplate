aa3261c9f0b4ac5a55706c649d2070db4a02c378
Amirhossein Jani <amir.jani500@gmail.com>
2020-11-03 10:56:34 +0330

    cleanup

--- app/crud/crud_country_language.py
+++ app/crud/crud_country_language.py
@@ -1,10 +1,16 @@
+from sqlalchemy.orm import Session
+
 from app.crud.base import CRUDBase
 from app.models.country_language import CountryLanguage
 from app.schemas import CountryLanguageCreate, CountryLanguageUpdate
 
 
 class CRUDCountryLanguage(CRUDBase[CountryLanguage, CountryLanguageCreate, CountryLanguageUpdate]):
-    pass
+    def get_one(self, db: Session, *, country_id: int, language_id: int):
+        return db.query(CountryLanguage).\
+            filter(CountryLanguage.country_id == country_id).\
+            filter(CountryLanguage.language_id == language_id).\
+            first()
 
 
 country_language = CRUDCountryLanguage(CountryLanguage)
--- app/db/init_db.py
+++ app/db/init_db.py
@@ -5,46 +5,44 @@ from app import crud, schemas
 from app.core.config import settings
 from app.db import base  # noqa: F401
 from app.helpers import curl
+from app.models import Country
 
 
 def init_db(db: Session) -> None:
-    # user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
-    # if not user:
-    #     user_in = schemas.UserCreate(
-    #         email=settings.FIRST_SUPERUSER,
-    #         password=settings.FIRST_SUPERUSER_PASSWORD,
-    #         is_superuser=True,
-    #     )
-    #     user = crud.user.create(db, obj_in=user_in)  # noqa: F841
-
     rest_country_url = 'https://restcountries.eu/rest/v2/all'
     countries = requests.get(rest_country_url, headers={"content-type": "application/json"}).json()
 
     for country in countries:
-        country_code = country.get('alpha2Code') if country.get('alpha2Code') else country.get('alpha3Code');
-        country_exists = crud.country.get_by_code(db, code=country_code)
+        country_code = get_country_code(country)
+        country_exists = get_existed_country(db, country_code)
         if not country_exists:
             seed_country(db, country, country_code)
 
         for language in country.get('languages'):
-            code = language.get('iso639_1') if language.get('iso639_1') else language.get('iso639_2')
+            code = get_language_code(language)
             language_exists = crud.language.get_by_code(db, code=code)
             if not language_exists:
                 seed_language(db, language, code)
 
         for language in country.get('languages'):
-            code = language.get('iso639_1') if language.get('iso639_1') else language.get('iso639_2')
-
-            country_code = country.get('alpha2Code') if country.get('alpha2Code') else country.get('alpha3Code');
-            country_exists = crud.country.get_by_code(db, code=country_code)
+            code = get_language_code(language)
+            country_code = get_country_code(country)
+            country_exists = get_existed_country(db, country_code)
 
             if country_exists:
-                language_to_attach = crud.language.get_by_code(db, code=code).id
-                country_language_in = schemas.CountryLanguageCreate(
-                    country_id=country_exists.id,
-                    language_id=language_to_attach
-                )
-                crud.country_language.create(db, obj_in=country_language_in)
+                country_language_seed(db, code, country_exists)
+
+
+def get_existed_country(db: Session, code: int):
+    return crud.country.get_by_code(db, code=code)
+
+
+def get_language_code(language: dict):
+    return language.get('iso639_1') if language.get('iso639_1') else language.get('iso639_2')
+
+
+def get_country_code(country: dict):
+    return country.get('alpha2Code') if country.get('alpha2Code') else country.get('alpha3Code')
 
 
 def seed_country(db: Session, country: dict, code: str):
@@ -66,3 +64,14 @@ def seed_language(db: Session, language: dict, code: str):
         native_name=language.get('nativeName')
     )
     crud.language.create(db, obj_in=language_in)
+
+
+def country_language_seed(db: Session, code: str, country_exists: Country):
+    language_to_attach = crud.language.get_by_code(db, code=code).id
+    country_language_exists = crud.country_language.get_one(db, country_id=country_exists.id, language_id=language_to_attach)
+    if not country_language_exists:
+        country_language_in = schemas.CountryLanguageCreate(
+            country_id=country_exists.id,
+            language_id=language_to_attach
+        )
+        crud.country_language.create(db, obj_in=country_language_in)
